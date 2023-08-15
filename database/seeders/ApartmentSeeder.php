417ec33378a1e362d527aa36dbdcfcd5c4252214
Aleksandar Milic <aleksandar.m@badsistems.com>
2023-08-15 18:17:10 +0200

    quick fixes on seeder

--- database/seeders/ApartmentSeeder.php
+++ database/seeders/ApartmentSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class ApartmentSeeder extends Seeder
 {
@@ -12,6 +13,17 @@ class ApartmentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Load data from JSON file
+        $data = json_decode(file_get_contents(database_path('seeds/list.json')), true);
+
+        // Insert data into the apartments table
+        foreach ($data as $apartment) {
+            // Check if "floor" is numeric
+            if (is_numeric($apartment['floor'])) {
+                $apartment['floor'] = (string) $apartment['floor'];
+            }
+            
+            DB::table('apartments')->insert($apartment);
+        }
     }
 }
