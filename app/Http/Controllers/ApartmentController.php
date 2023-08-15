2ed7d2e7273629f44a8f2b0dfbf5b762ff6c7c62
Aleksandar Milic <aleksandar.m@badsistems.com>
2023-08-15 18:06:02 +0200

    apartment model, seeds and routes fix

---
+++ app/Http/Controllers/ApartmentController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Apartment;
+
+
+class ApartmentController extends Controller
+{
+    public function index()
+    {
+        $apartments = Apartment::all();
+        return response()->json(['apartments' => $apartments]);
+    }
+
+    public function show($id)
+    {
+        $apartment = Apartment::find($id);
+        if (!$apartment) {
+            return response()->json(['message' => 'Apartment not found'], 404);
+        }
+        return response()->json(['apartment' => $apartment]);
+    }
+    
+    public function store(Request $request)
+    {
+        $apartment = Apartment::create($request->all());
+        return response()->json(['message' => 'Apartment created', 'apartment' => $apartment], 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $apartment = Apartment::find($id);
+        if (!$apartment) {
+            return response()->json(['message' => 'Apartment not found'], 404);
+        }
+        $apartment->update($request->all());
+        return response()->json(['message' => 'Apartment updated', 'apartment' => $apartment]);
+    }
+
+    public function destroy($id)
+    {
+        $apartment = Apartment::find($id);
+        if (!$apartment) {
+            return response()->json(['message' => 'Apartment not found'], 404);
+        }
+        $apartment->delete();
+        return response()->json(['message' => 'Apartment deleted']);
+    }
+}
